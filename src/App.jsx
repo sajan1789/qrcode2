d971a18597c197ad93fd524eba2ab556d8670836
sajan1789 <sajan99559@gmail.com>
2025-04-17 00:28:12 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 // import Menu from './components/Menu';
 import QRCodeComponent from './components/Qr_code';
+import Menu from './components/Menu';
 
 
 const App = () => {
